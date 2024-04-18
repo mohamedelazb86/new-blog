f333913df81687cbe7badc9ee8ba30f96ca57554
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2024-04-18 10:09:09 +0200

    create database

---
+++ posts/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+from django.utils.text import slugify
+from django.utils import timezone
+from taggit.managers import TaggableManager
+
+
+
+class Post(models.Model):
+    user=models.ForeignKey(User,related_name='post_user',on_delete=models.SET_NULL,null=True,blank=True)
+    title=models.CharField(max_length=120,verbose_name=_('title'))
+    content=models.TextField(max_length=1000)
+    draft=models.BooleanField(default=True)
+    tags = TaggableManager()
+    image=models.ImageField(upload_to='photo_post')
+    publish_date=models.DateTimeField(default=timezone.now)
+    category=models.ForeignKey('Category',related_name='post_category',on_delete=models.SET_NULL,null=True,blank=True)
+    slug=models.SlugField(null=True,blank=True)
+
+
+    def __str__(self):
+        return self.title
+    
+    def save(self,*args,**kwargs):
+        self.slug= slugify(self.title)
+        super(Post,self).save(*args,**kwargs)
+
+class Category(models.Model):
+    name=models.CharField(max_length=120)
+
+    def _str__(self):
+        return self.name
+    
+class Comment(models.Model):
+    user=models.CharField(max_length=120)
+    post=models.ForeignKey(Post,related_name='comment_post',on_delete=models.CASCADE)
+    publish_date=models.DateTimeField(default=timezone.now)
+    rate=models.IntegerField(choices=[(i,i) for i in range(1,6)])
+    conent=models.TextField(max_length=350)
+
+    def __str__(self):
+        return f'{self.user}---{self.post}'
+
+
+
+
--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    # my package
+    "taggit",
+
+    # my apps
+
+    'posts',
 ]
 
 MIDDLEWARE = [
